e4c3b29f537d5220ab36e29d3afdb25bed46f77d
Martín Rincón <mrinconnarcia@gmail.com>
2024-02-25 12:27:39 -0600

    up login and signup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movil_project/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RuralMarketExpress',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime.shade50),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
